c346b1d5554a80d8b6a7b821855b25ef35d3ec03
Rey <padilla.reyj@gmail.com>
2013-02-27 21:24:08 +0700

    Some finishing touches

--- www/bump.js
+++ www/bump.js
@@ -15,10 +15,22 @@ window.bumpable = function(str, callback) {
 
 
  
-
-function callBack(data){
-     alert(data);
+/*
+ Receives Data when a bump is match. Check Plugin/ios/Bump.m 
+ */
+function matchResult(data){
+    //alert(data);
+    document.getElementById("result").innerHTML = data + '<br>' + document.getElementById("result").innerHTML;
 }
 
+
+/*
+ Receives Data like no match, not connected, connected and bump detected 
+ Update Bump.m to your liking
+ */
+function updateContent(data){
+     //alert(data);
+    document.getElementById("result").innerHTML = data + '<br>' + document.getElementById("result").innerHTML;
+}
    
     
